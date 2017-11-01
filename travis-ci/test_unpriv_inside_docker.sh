78b8eae99812c4d5e9eae1b2f5e70ccfe39eaaa6
Matyas Selmeci <matyas@cs.wisc.edu>
2017-11-01 08:53:58 -0500

    Undummy timeout-test but decrease the timeout

--- travis-ci/test_unpriv_inside_docker.sh
+++ travis-ci/test_unpriv_inside_docker.sh
@@ -9,11 +9,4 @@ make
 
 env | sort
 
-# timeout-test.pl times out travis so dummy it out
-cat > xio/src/test/timeout-test.pl <<'__EOF__'
-#!/usr/bin/perl
-print "THIS TEST IS DUMMIED OUT FOR TRAVIS-CI RUNS\n";
-exit 0
-__EOF__
-
 make -j1 check | tee check.out
